b17ccbd1e2e70223859b6a2b5b3d090e9c578b67
stillouyng <sapranovmihail1@icloud.com>
2025-04-10 08:33:08 -0700

    Change dependencies

--- tests/utf8.rs
+++ tests/utf8.rs
@@ -1,5 +1,6 @@
 #[cfg(test)]
 mod tests {
+    use rand::{Rng, thread_rng};
     use crypto_playground::encode::encode;
 
     #[test]
@@ -10,11 +11,10 @@ mod tests {
 
     #[test]
     fn random_unicode_stress_test() {
-        use rand::Rng;
-        let mut rng = rand::rng();
+        let mut rng = thread_rng();
 
         for _ in 0..10000 {
-            let code = rng.random_range(0..=0x10FFFF);
+            let code = rng.gen_range(0..=0x10FFFF);
             if let Some(c) = char::from_u32(code) {
                 let s = c.to_string();
                 assert_eq!(encode(&s), s.as_bytes());
