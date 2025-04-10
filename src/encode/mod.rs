2d332e2f47c3f56e27cdbb93819bad5ae29e55bc
stillouyng <sapranovmihail1@icloud.com>
2025-04-10 08:34:09 -0700

    Add aes module

--- src/encode/mod.rs
+++ src/encode/mod.rs
@@ -1,8 +1,11 @@
 mod utf8;
 mod xor;
+mod aes256;
+
 
 pub use utf8::encode;
 pub use xor::{xor_encrypt, generate_key};
+pub use aes256::gf256;
 
 pub fn encoding(data: &str, use_xor: bool) -> Vec<u8> {
     let bytes = encode(data);
