1dd30efd3e9d8e5d36f703b63a5166527e8e05f9
stillouyng <sapranovmihail1@icloud.com>
2025-04-10 14:26:58 -0700

    Create affine_transform for SBox

---
+++ src/encode/aes256/sbox.rs
@@ -0,0 +1,31 @@
+use crate::encode::gf256::GF256;
+
+pub struct SBox {
+    pub table: [u8; 256],
+    // pub inverse: [u8; 256],
+}
+
+impl SBox {
+    pub fn new() -> Self {
+        let mut table = [0; 256];
+        for a in 0..=255 {
+            let inv = GF256::inverse(a);
+            table[a as usize] = Self::affine_transform(inv);
+        }
+        SBox { table }
+    }
+
+    pub fn affine_transform(byte: u8) -> u8 {
+        let mut result = 0u8;
+        for i in 0..8 {
+            let bit = ((byte >> i) & 1) ^
+                ((byte >> ((i + 4) % 8)) & 1) ^
+                ((byte >> ((i + 5) % 8)) & 1) ^
+                ((byte >> ((i + 6) % 8)) & 1) ^
+                ((byte >> ((i + 7) % 8)) & 1) ^
+                ((0x63 >> i) & 1);
+            result |= bit << i;
+        }
+        result
+    }
+}
