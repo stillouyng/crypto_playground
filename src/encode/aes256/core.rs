1e972c6f754e5c649b20a005c32742ecb0bdd21b
stillouyng <sapranovmihail1@icloud.com>
2025-04-11 21:44:36 -0700

    Add add_round_key and sub_bytes (finally AES256 is completed!)

--- src/encode/aes256/core.rs
+++ src/encode/aes256/core.rs
@@ -13,6 +13,14 @@ impl AES {
         }
     }
 
+    pub fn sub_bytes(&self, state: &mut [[u8; 4]; 4]) {
+        for row in state.iter_mut() {
+            for byte in row.iter_mut() {
+                *byte = self.sbox.substitute(*byte);
+            }
+        }
+    }
+
     pub fn shift_rows(&self, state: &mut [[u8; 4]; 4]) {
         for i in 1..4 {
             state[i].rotate_left(i);
@@ -30,6 +38,14 @@ impl AES {
             state[1][col] = a0 ^ GF256::mul(0x02, a1) ^ GF256::mul(0x03, a2) ^ a3;
             state[2][col] = a0 ^ a1 ^ GF256::mul(0x02, a2) ^ GF256::mul(0x03, a3);
             state[3][col] = GF256::mul(0x03, a0) ^ a1 ^ a2 ^ GF256::mul(0x02, a3);
+        }
+    }
+
+    pub fn add_round_key(&self, state: &mut [[u8; 4]; 4], round_key: &[u8; 16]) {
+        for col in 0..4 {
+            for row in 0..4 {
+                state[row][col] ^= round_key[col * 4 + row];
+            }
         }
     }
 }
