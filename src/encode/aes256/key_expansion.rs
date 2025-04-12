a1ea12c8674bce2e0116ddf3a013a6de83e6accc
stillouyng <sapranovmihail1@icloud.com>
2025-04-11 20:32:15 -0700

    Add main key_expansion function

--- src/encode/aes256/key_expansion.rs
+++ src/encode/aes256/key_expansion.rs
@@ -3,7 +3,7 @@ use crate::utils::{sub_word, rot_word, xor_words};
 
 pub struct KeyExpansion {
     sbox: SBox,
-    rcon: [u8; 10]
+    rcon: [u8; 14]
 }
 
 
@@ -11,7 +11,10 @@ impl KeyExpansion {
     pub fn new() -> Self {
         KeyExpansion {
             sbox: SBox::new(),
-            rcon: [0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1B, 0x36]
+            rcon: [
+                0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40,
+                0x80, 0x1B, 0x36, 0x6C, 0xD8, 0xAB, 0x4D
+            ],
         }
     }
 
@@ -19,27 +22,34 @@ impl KeyExpansion {
         let mut words = Vec::with_capacity(60);
 
         for i in 0..8 {
-            words.push([key[4*i], key[4*i+1], key[4*i+2], key[4*i+3]]);
+            words.push([
+                key[4 * i],
+                key[4 * i + 1],
+                key[4 * i + 2],
+                key[4 * i + 3],
+            ]);
         }
 
+        // Генерация оставшихся слов
         for i in 8..60 {
-            let mut temp = words[i-1];
+            let mut temp = words[i - 1];
 
             if i % 8 == 0 {
                 temp = sub_word(&self.sbox, rot_word(temp));
-                temp[0] ^= self.rcon[i/8 - 1];
+                temp[0] ^= self.rcon[i / 8 - 1];
             } else if i % 8 == 4 {
                 temp = sub_word(&self.sbox, temp);
             }
 
-            let new_word = xor_words(words[i-8], temp);
+            let new_word = xor_words(words[i - 8], temp);
             words.push(new_word);
         }
 
-        (0..15).map(|i| {
-            let mut round_key = [0; 16];
+        (0..15).map(|round| {
+            let mut round_key = [0u8; 16];
             for j in 0..4 {
-                round_key[4*j..4*j+4].copy_from_slice(&words[4*i + j]);
+                let word_idx = round * 4 + j;
+                round_key[4 * j..4 * j + 4].copy_from_slice(&words[word_idx]);
             }
             round_key
         }).collect()
